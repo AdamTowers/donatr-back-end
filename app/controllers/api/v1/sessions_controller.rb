a4ed13eb5139f4296dd734e811d7c9693de1f0df
adamtowers <hello@adamtowers.design>
2018-06-06 18:07:33 -0400

    created two separate methods in sessions controller for both user types

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -3,13 +3,24 @@ module Api
 
     class SessionsController < ApplicationController
 
-      def create
-        @user = User.find_by(username: params["username"])
+      def create_donor
+        @user = Donor.find_by(username: params["username"])
         if (@user && @user.authenticate(params["password"]))
           render json: token_json(@user)
         else
           render json: {
-            errors: "Those credentials don't match anything we have in our database"
+            errors: "Those credentials don't match anything we have in our database."
+          }, status: :unauthorized
+        end
+      end
+
+      def create_organization
+        @user = Organization.find_by(username: params["username"])
+        if (@user && @user.authenticate(params["password"]))
+          render json: token_json(@user)
+        else
+          render json: {
+            errors: "Those credentials don't match anything we have in our database."
           }, status: :unauthorized
         end
       end
