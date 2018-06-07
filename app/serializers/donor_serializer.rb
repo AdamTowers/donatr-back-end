615b19d5b223b5c68e19e8a898e848405de22e2b
adamtowers <hello@adamtowers.design>
2018-06-07 14:18:52 -0400

    added :email to donor serializer

--- app/serializers/donor_serializer.rb
+++ app/serializers/donor_serializer.rb
@@ -1,5 +1,5 @@
 class DonorSerializer < ActiveModel::Serializer
-  attributes :id, :username, :first_name, :last_name
+  attributes :id, :username, :first_name, :last_name, :email
 
   # To show all donations, uncomment the code below
   # has_many :donations
