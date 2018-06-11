9526026856949e6dc5da668cce8417038b4e0eb6
adamtowers <hello@adamtowers.design>
2018-06-11 15:01:49 -0400

    added funds_controller and route

---
+++ app/controllers/api/v1/funds_controller.rb
@@ -0,0 +1,17 @@
+module Api
+  module V1
+
+    class FundsController < ApplicationController
+      def index
+        @funds = Fund.all
+          render json: @funds
+      end
+
+      # private
+      # def fund_params
+      #   params.permit(:id, :username, :name, :bio, :email, :password, :password_digest)
+      # end
+    end
+
+  end
+end
