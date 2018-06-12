1c4f19822d32ad90d74e8f9c8febe6560e742acd
adamtowers <hello@adamtowers.design>
2018-06-12 11:06:55 -0400

    created donation_serializer#fund_name

--- app/serializers/donation_serializer.rb
+++ app/serializers/donation_serializer.rb
@@ -1,3 +1,7 @@
 class DonationSerializer < ActiveModel::Serializer
-  attributes :id, :donor_id, :fund_id, :amount
+  attributes :id, :donor_id, :fund_id, :fund_name, :amount
+
+  def fund_name
+    object.fund.title
+  end
 end
