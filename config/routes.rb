7430ffbcbb5b895d5936c1daae75cc75de4c7c53
adamtowers <hello@adamtowers.tech>
2018-07-26 22:07:48 -0400

    created donors_controller#edit and organizations_controller#edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
    namespace :v1 do
-     resources :donors, only: [:show, :create, :update]
+     resources :donors, only: [:show, :create, :edit, :update]
      resources :donations, only: [:create]
-     resources :organizations, only: [:index, :show, :create, :update]
+     resources :organizations, only: [:index, :show, :create, :edit, :update]
      resources :funds, only: [:index, :show, :create]
 
      post '/donor_sessions/', to: 'sessions#create_donor_sessions', as: 'donor_sessions'
